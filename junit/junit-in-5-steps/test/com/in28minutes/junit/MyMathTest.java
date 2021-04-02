145f863e714a07337baba2142139a68a402eb689
joconstantine <joconstantine@users.noreply.github.com>
2021-04-02 23:17:23 +0800

    02/04/2021 15:00

--- junit/junit-in-5-steps/test/com/in28minutes/junit/MyMathTest.java
+++ junit/junit-in-5-steps/test/com/in28minutes/junit/MyMathTest.java
@@ -1,29 +1,46 @@
 package com.in28minutes.junit;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.Assert.assertEquals;
 
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 class MyMathTest {
+	MyMath myMath = new MyMath();
+	
+	@BeforeAll
+	public static void before() {
+		System.out.println("Before");
+	}
+	
+	@AfterAll
+	public static void after() {
+		System.out.println("After");
+	}
+	
+	@BeforeEach
+	public void beforeClass() {
+		System.out.println("BeforeClass");
+	}
+	
+	@AfterEach
+	public void afterClass() {
+		System.out.println("AfterClass");
+	}
 
 	@Test
 	void sum_with3numbers() {
-		MyMath myMath = new MyMath();
-		int result = myMath.sum(new int[] {1, 2, 3});
-		
-		assertEquals(6, result);
-		
-		System.out.println(result);
+		System.out.println("Test1");
+		assertEquals(6, myMath.sum(new int[] {1, 2, 3}));
 	}
 	
 	@Test
 	void sum_with1number1() {
-		MyMath myMath = new MyMath();
-		int result = myMath.sum(new int[] {1});
-		
-		assertEquals(1, result);
-		
-		System.out.println(result);
+		System.out.println("Test1");
+		assertEquals(1, myMath.sum(new int[] {1}));
 	}
 
 }
