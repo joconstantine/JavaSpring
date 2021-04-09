05ab9ca075d1cb78225b1d157c3af3d439d9e5d0
constantine <constantine@NCS-010618GM04.vrldevdm1a.local>
2021-04-09 10:25:31 +0800

    09/04/2021 10:25

---
+++ first-web-application/first-web-application/src/main/java/webapp/jee/UserValidationService.java
@@ -0,0 +1,12 @@
+package webapp.jee;
+
+public class UserValidationService {
+	
+	public boolean isUserValid(String user, String password) {
+		if (user.equals("in28Minutes") && password.equals("dummy")) {
+			return true;
+		}
+		
+		return false;
+	}
+}
