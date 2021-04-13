7a989d9938d82141cb7657c58ffc9c52aac35c5e
Constantine Nguyen <constantine@ncs.com.sg>
2021-04-13 15:58:44 +0800

    13/04/2021 15:58

---
+++ jpa-in-depth/jpa-in-depth/src/main/java/com/in28minutes/jpa/hibernate/demo/DemoApplication.java
@@ -0,0 +1,25 @@
+package com.in28minutes.jpa.hibernate.demo;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import com.in28minutes.jpa.hibernate.demo.repository.CourseRepository;
+
+@SpringBootApplication
+public class DemoApplication implements CommandLineRunner{
+
+	@Autowired
+	CourseRepository reposity;
+	
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		reposity.findById(10001L);
+	}
+
+}
