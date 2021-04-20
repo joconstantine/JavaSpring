9918295e1c779f151f4999508ce300ea427e052e
Constantine Nguyen <constantine@ncs.com.sg>
2021-04-20 17:22:54 +0800

    20/04/2021 - 17:22

---
+++ spring-boot-web-application/spring-boot-first-application/src/main/java/com/in28minutes/springboot/web/SpringBootFirstApplication.java
@@ -0,0 +1,13 @@
+package com.in28minutes.springboot.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootFirstApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootFirstApplication.class, args);
+	}
+
+}
---
+++ spring-boot-web-application/spring-boot-first-application/src/test/java/com/in28minutes/springboot/web/springbootfirstapplication/SpringBootFirstApplicationTests.java
@@ -0,0 +1,13 @@
+package com.in28minutes.springboot.web.springbootfirstapplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootFirstApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
