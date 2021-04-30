0c14eaa00bd0699b9caad2a9abbb98ab5cf50dfa
Constantine Nguyen <constantine@ncs.com.sg>
2021-04-30 12:30:32 +0800

    30/04/2021 12:30

---
+++ spring-boot-deep-dive/first-springboot-project/src/main/java/com/in28minutes/springboot/controller/WelcomeController.java
@@ -0,0 +1,19 @@
+package com.in28minutes.springboot.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.in28minutes.springboot.service.WelcomeService;
+
+@RestController
+public class WelcomeController {
+
+	@Autowired
+	private WelcomeService service;
+
+	@RequestMapping("/welcome")
+	public String welcome() {
+		return service.retrieveWelcomeMessage();
+	}
+}
