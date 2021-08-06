a48b192bb6d7ea6cd84eaf59130cbc58e1e6b28c
joconstantine <joconstantine@users.noreply.github.com>
2021-08-06 14:44:35 +0800

    06/08/2021 14:44

--- Luv2Code/dev-spring/spring-demo-aop/src/com/luv2code/aopdemo/MainDemoApp.java
+++ Luv2Code/dev-spring/spring-demo-aop/src/com/luv2code/aopdemo/MainDemoApp.java
@@ -3,26 +3,27 @@ package com.luv2code.aopdemo;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 import com.luv2code.aopdemo.dao.AccountDAO;
+import com.luv2code.aopdemo.dao.MembershipDAO;
 
 public class MainDemoApp {
 
 	public static void main(String[] args) {
-		
+
 		// read spring configuration java class
-		AnnotationConfigApplicationContext context
-			= new AnnotationConfigApplicationContext(DemoConfig.class);
-		
-		//get the bean from spring container
+		AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(DemoConfig.class);
+
+		// get the bean from spring container
 		AccountDAO theAccountDAO = context.getBean("accountDAO", AccountDAO.class);
-		
+		MembershipDAO theMembershipDAO = context.getBean("membershipDAO", MembershipDAO.class);
+
 		// call the business method
-		theAccountDAO.addAccount();
-		
+		theAccountDAO.addAccount(new Account(), true);
+		theAccountDAO.doWork();
+
 		// do it again!
-		System.out.println("\n let's call it again!\n");
-		
-		theAccountDAO.addAccount();
-		
+		theMembershipDAO.addMembership();
+		theMembershipDAO.goToSleep();
+
 		// close the context
 		context.close();
 
--- Luv2Code/dev-spring/spring-demo-aop/src/com/luv2code/aopdemo/aspect/MyDemoLoggingAspect.java
+++ Luv2Code/dev-spring/spring-demo-aop/src/com/luv2code/aopdemo/aspect/MyDemoLoggingAspect.java
@@ -9,11 +9,11 @@ import org.springframework.stereotype.Component;
 public class MyDemoLoggingAspect {
 
 	// this is where we add all of our related advices for logging
-	
+
 	// let's start with @Before advice
-	@Before("execution(public void addAccount())")
+	@Before("execution(* com.luv2code.aopdemo.dao.*.*(..))")
 	public void beforeAddAccountAdvice() {
-		
+
 		System.out.println("\n=======>>> Executing @Before advice on addAccount()");
 	}
 }
