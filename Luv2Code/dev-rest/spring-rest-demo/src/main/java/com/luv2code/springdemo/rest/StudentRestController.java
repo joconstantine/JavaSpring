99d30110937b3dfa1a102b905cd1a0a341aaf8e0
joconstantine <joconstantine@users.noreply.github.com>
2021-08-30 23:29:38 +0800

    30/08/2021

--- Luv2Code/dev-rest/spring-rest-demo/src/main/java/com/luv2code/springdemo/rest/StudentRestController.java
+++ Luv2Code/dev-rest/spring-rest-demo/src/main/java/com/luv2code/springdemo/rest/StudentRestController.java
@@ -34,6 +34,12 @@ public class StudentRestController {
 
 	@GetMapping("/students/{studentId}")
 	public Student getStudent(@PathVariable int studentId) {
+
+		// check the studentId against the list size
+		if (studentId >= theStudents.size()) {
+			throw new StudentNotFoundException("Student id not found - " + studentId);
+		}
+
 		return theStudents.get(studentId);
 	}
 
